22bb59e24ec92ba49ccfb1d8dcebfb19bf68fba1
Elzenif <shigo.germain@gmail.com>
2016-01-30 13:50:31 +0100

    testWeaponTypeIsValid

--- TESGenerator/src/test/WeaponTest.java
+++ TESGenerator/src/test/WeaponTest.java
@@ -1,6 +1,12 @@
 package test;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import main.enums.WeaponType;
 import main.objects.Weapon;
 
 import org.junit.Test;
@@ -8,16 +14,28 @@ import org.junit.Test;
 public class WeaponTest {
 
 	@Test
-	public final void testWeaponNameNotNull() {
+	public final void testWeaponNameisNotNull() {
 		Weapon weapon = new Weapon();
 		weapon.initialize();
 		assertNotNull("The weapon name should not be null", weapon.getName());
 	}
 
 	@Test
-	public final void testWeaponTypeNotNull() {
+	public final void testWeaponTypeIsNotNull() {
 		Weapon weapon = new Weapon();
 		weapon.initialize();
 		assertNotNull("The weapon type should not be null", weapon.getType());
+	}
+	
+	@Test
+	public final void testWeaponTypeIsValid() {
+		Weapon weapon = new Weapon();
+		weapon.initialize();
+		Set<WeaponType> expected = new HashSet<WeaponType>(Arrays.asList(WeaponType.values()));
+		Set<WeaponType> actual = new HashSet<WeaponType>();
+		for (WeaponType wt : WeaponType.values()) {
+			actual.add(wt);
+		}
+		assertEquals(expected, actual);
 	}
 }
