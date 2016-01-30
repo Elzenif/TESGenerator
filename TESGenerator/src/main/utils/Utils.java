7d84021d7da3988b37967e924cefba9c0c475067
Elzenif <shigo.germain@gmail.com>
2016-01-30 16:04:54 +0100

    testSumSet

--- TESGenerator/src/main/utils/Utils.java
+++ TESGenerator/src/main/utils/Utils.java
@@ -1,11 +1,23 @@
 package main.utils;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class Utils {
 
 	public static int sum(int[] array) {
 		int result = 0;
 		for (int i = 0; i < array.length; i++) {
 			result += array[i];
+		}
+		return result;
+	}
+	
+	public static int sum(Set<Integer> set) {
+		int result = 0;
+		Iterator<Integer> it = set.iterator();
+		while(it.hasNext()) {
+			result += it.next();
 		}
 		return result;
 	}
--- TESGenerator/src/test/utils/UtilsTest.java
+++ TESGenerator/src/test/utils/UtilsTest.java
@@ -1,6 +1,11 @@
 package test.utils;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import main.utils.Utils;
 
 import org.junit.Test;
@@ -10,7 +15,13 @@ public class UtilsTest {
 	@Test
 	public void testSumArray() {
 		int[] array = {1,3,15,2,9};
-		assertEquals(30, Utils.sum(array));
+		assertEquals("The result is incorrect", 30, Utils.sum(array));
 	}
 
+	@Test
+	public void testSumSet() {
+		Integer[] array = {1,3,15,2,9};
+		Set<Integer> set = new HashSet<Integer>(Arrays.asList(array));
+		assertEquals("The result is incorrect", 30, Utils.sum(set));
+	}
 }
