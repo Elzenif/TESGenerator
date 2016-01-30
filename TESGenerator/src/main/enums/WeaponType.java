c5c055e9813b98d5c23bac4424b398184d453aa9
Elzenif <shigo.germain@gmail.com>
2016-01-30 15:01:54 +0100

    testOneOrTwoHand

--- TESGenerator/src/main/enums/WeaponType.java
+++ TESGenerator/src/main/enums/WeaponType.java
@@ -2,6 +2,35 @@ package main.enums;
 
 public enum WeaponType {
 	
-	SWORD;
+	BATTLE_AXE("battle axe", 2),
+	BOW("bow", 2),
+	CLAYMORE("claymore", 2),
+	CUTLASS("cutlass", 1),
+	DAGGER("dagger", 1),
+	KATANA("katana", 1),
+	MACE("mace", 1),
+	STAFF("staff", 1),
+	SWORD("sword", 1),
+	WAR_AXE("war axe", 1),
+	WARHAMMER("warhammer", 2);
 
+	private String name;
+	private int nbHands;
+	
+	private WeaponType(String name, int nbHands) {
+		this.name = name;
+		this.nbHands = nbHands;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getNbHands() {
+		return nbHands;
+	}
+	
+	public String toString() {
+		return name + " (" + nbHands + " hand(s))";
+	}
 }
--- TESGenerator/src/test/enums/WeaponTypeTest.java
+++ TESGenerator/src/test/enums/WeaponTypeTest.java
@@ -1,14 +1,25 @@
 package test.enums;
 
 import static org.junit.Assert.*;
+import main.enums.WeaponType;
 
 import org.junit.Test;
 
 public class WeaponTypeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public final void testNameIsNotNull() {
+		for (WeaponType wt : WeaponType.values()) {
+			assertNotNull("The weapon type name should not be null",
+					wt.getName());
+		}
 	}
 
+	@Test
+	public final void testOneOrTwoHand() {
+		for (WeaponType wt : WeaponType.values()) {
+			assertTrue("The weapon type should be one or two hands", 
+					wt.getNbHands() == 1 || wt.getNbHands() == 2);
+		}
+	}
 }
