ace9e87cf8da632707c223091827ae30bff49077
Elzenif <shigo.germain@gmail.com>
2016-01-30 18:11:50 +0100

    Superclass Item for Weapon

---
+++ TESGenerator/src/main/items/Item.java
@@ -0,0 +1,15 @@
+package main.items;
+
+public class Item {
+
+	protected String name;
+
+	public String getName() {
+		return name;
+	}
+
+	protected Object pickRandomItemType() {
+		
+		return null;
+	}
+}
